152fdf5d6300c3361adddabbba07c3eb14cd01c7
toketin <marco@vivopicenin.localdomain>
2021-11-11 19:56:59 +0100

    remove old snapshot

---
+++ restic/README.md
@@ -0,0 +1,2 @@
+## Backup sul raspberry (raspbian)
+Eseguo ogni venerdì lo script `restic.sh` aggiungendolo in crontab (`crontab -e` da root) `0 3 * * 5 /path/restic.sh` 
--- restic/restic.sh
+++ restic/restic.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 restic -r /srv/restic-repo backup / --password-file /path/restic_passwd --exclude-file /path/exclude.dir
 chown -R marco:marco /srv/restic-repo
+restic forget --keep-daily 4 --prune
